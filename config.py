7f5717417cc9e2e56fd1dde97a98333c8044d3e3
Harshitgupta0001 <166941860+Harshitgupta0001@users.noreply.github.com>
2024-09-11 22:32:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "25492855")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "61876db014de51a4ace6b169608be4f1") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6842691313:AAGBTMDh4bxADkw9YLosMErobc1vLASg-uE") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002166149059') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
@@ -18,7 +18,7 @@ class Config(object):
 
     # Other Configs 
     ADMIN = int(os.environ.get("ADMIN", "6359874284")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002155023199')) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002265434094')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
     START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
 
